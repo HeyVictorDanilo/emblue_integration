cea04729947ee0440f3d4aea9a17d8d8c4d43476
Victor Castaneda <victor.castaneda@hey.net.co>
2022-07-18 19:36:06 +0100

    Structure

--- main.py
+++ main.py
@@ -1,5 +1,32 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+from database import main_db
+import pysftp
+from datetime import date
+
+
+class EmblueConnection(pysftp.Connection):
+    def __init__(self, *args, **kwargs):
+        self._sftp_live = False
+        self._transport = None
+        super().__init__(*args, **kwargs)
+
+
+class Emblue:
+    def __init__(self):
+        self.db_instance = main_db.DBInstance(public_key="kKS0DfTKpE8TqUZs")
+
+    def get_emblue_accounts(self):
+        accounts = self.db_instance.handler(query="SELECT * FROM em_blue;")
+        return accounts
+
+    def download_file(self):
+        for account in self.get_emblue_accounts():
+            with EmblueConnection(account[2], username=account[4], password=account[3]) as sftp:
+                print(sftp.pwd())
+                with sftp.cd('upload/Report'):
+                    today = date.today().strftime("%Y%m%d")
+                    sftp.get(f"ACTIVIDADDETALLEDIARIOFTP{today}.zip")
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    emblue = Emblue()
+    emblue.download_file()
