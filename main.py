d0427a3ac56ee2c9968e8b839ea98f68bebe46cd
Victor Castaneda <victor.castaneda@hey.net.co>
2022-07-19 01:54:25 +0100

    Add unzip and reader method.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from database import main_db
 import pysftp
 from datetime import date
+import zipfile
+import time
+import csv
+import pandas as pd
 
 
 class EmblueConnection(pysftp.Connection):
@@ -13,6 +17,7 @@ class EmblueConnection(pysftp.Connection):
 class Emblue:
     def __init__(self):
         self.db_instance = main_db.DBInstance(public_key="kKS0DfTKpE8TqUZs")
+        self.today = date.today().strftime("%Y%m%d")
 
     def get_emblue_accounts(self):
         accounts = self.db_instance.handler(query="SELECT * FROM em_blue;")
@@ -21,12 +26,28 @@ class Emblue:
     def download_file(self):
         for account in self.get_emblue_accounts():
             with EmblueConnection(account[2], username=account[4], password=account[3]) as sftp:
-                print(sftp.pwd())
                 with sftp.cd('upload/Report'):
-                    today = date.today().strftime("%Y%m%d")
-                    sftp.get(f"ACTIVIDADDETALLEDIARIOFTP{today}.zip")
+                    sftp.get(f"ACTIVIDADDETALLEDIARIOFTP_{self.today}.zip")
+
+    def unzip_local_file(self):
+        try:
+            with zipfile.ZipFile(f"ACTIVIDADDETALLEDIARIOFTP_{self.today}.zip", mode="r") as archive:
+                archive.extractall()
+        except zipfile.BadZipFile as error:
+            raise error
+
+    def read_local_file(self):
+        with open(f"ACTIVIDADDETALLEDIARIOFTP_{self.today}.csv", 'r') as csvfile:
+            csvreader = csv.reader(csvfile)
+            for row in csvreader:
+                print(row)
 
 
 if __name__ == '__main__':
+    start = time.time()
     emblue = Emblue()
     emblue.download_file()
+    emblue.unzip_local_file()
+    emblue.read_local_file()
+    end = time.time()
+    print(end - start)
