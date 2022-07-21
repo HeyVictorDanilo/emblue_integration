6dab45dd227ea1051ccd2dda0ac3d15ea387cf46
Victor Castaneda <victor.castaneda@hey.net.co>
2022-07-21 22:27:27 +0100

    Before running database hit.

--- main.py
+++ main.py
@@ -73,48 +73,60 @@ class Emblue:
 
         for line in lines:
             line_words = line.split(";")
+            if not line_words[8]:
+                tag = "NULL"
+            else:
+                tag = line_words[8]
 
             if line_words[6] == "Enviado":
                 sent_values_list.append(
                     (
+                        line_words[0],
                         line_words[1],
                         line_words[2],
                         line_words[3],
                         line_words[4],
-                        line_words[7]
+                        line_words[7],
+                        tag
                     )
                 )
 
             if line_words[6] == "Click":
                 click_values_list.append(
                     (
+                        line_words[0],
                         line_words[1],
                         line_words[2],
                         line_words[3],
                         line_words[4],
-                        line_words[7]
+                        line_words[7],
+                        tag
                     )
                 )
 
             if line_words[6] == "Abierto":
                 open_values_list.append(
                     (
+                        line_words[0],
                         line_words[1],
                         line_words[2],
                         line_words[3],
                         line_words[4],
-                        line_words[7]
+                        line_words[7],
+                        tag
                     )
                 )
 
             if line_words[6] == "Desuscripto":
                 unsubscribe_values_list.append(
                     (
+                        line_words[0],
                         line_words[1],
                         line_words[2],
                         line_words[3],
                         line_words[4],
-                        line_words[7]
+                        line_words[7],
+                        tag
                     )
                 )
 
@@ -125,64 +137,72 @@ class Emblue:
             build_insert_sent_query = self.build_insert_query(
                 table="em_blue_receive_email_event",
                 columns=[
+                    "email",
                     "sent_date",
                     "activity_date",
                     "campaign",
-                    "subject_campaign",
+                    "action",
                     "description",
+                    "tag"
                 ],
                 values=sent_values_list,
             )
-            print(build_insert_sent_query)
+            self.db_instance.handler(query=build_insert_sent_query)
 
         if click_values_list:
             build_insert_click_query = self.build_insert_query(
                 table="em_blue_link_click_event",
                 columns=[
+                    "email",
                     "sent_date",
                     "activity_date",
                     "campaign",
-                    "subject_campaign",
+                    "action",
                     "url",
+                    "tag"
                 ],
                 values=click_values_list,
             )
-            print(build_insert_click_query)
+            self.db_instance.handler(query=build_insert_click_query)
 
         if open_values_list:
             build_insert_open_query = self.build_insert_query(
                 table="em_blue_open_email_event",
                 columns=[
+                    "email",
                     "sent_date",
                     "activity_date",
                     "campaign",
-                    "subject_campaign",
+                    "action",
                     "description",
+                    "tag"
                 ],
                 values=open_values_list,
             )
-            print(build_insert_open_query)
+            self.db_instance.handler(query=build_insert_open_query)
 
         if unsubscribe_values_list:
             build_insert_unsubscribe_query = self.build_insert_query(
                 table="em_blue_unsubscribe_event",
                 columns=[
+                    "email",
                     "sent_date",
                     "activity_date",
                     "campaign",
-                    "subject_campaign",
+                    "action",
                     "description",
+                    "tag"
                 ],
                 values=unsubscribe_values_list,
             )
-            print(build_insert_unsubscribe_query)
+            self.db_instance.handler(query=build_insert_unsubscribe_query)
 
     @staticmethod
     def build_insert_query(table: str, columns: List[str], values: List[Any]) -> str:
         return f"""
             INSERT INTO {table}({", ".join([str(c) for c in columns])})
             VALUES {values};
-        """
+        """.replace("[", "").replace("]", "")
 
     def execute(self):
         self.download_file()
